a5c640ac20440986859bf6d2fefa556c60668805
Yayo <perez.nava.yael.mauricio.ipn@gmail.com>
2024-06-07 13:51:13 -0600

    intento 2 de hacer funcionar el registro

--- Eatech/wwwroot/js/INST_PAD.js
+++ Eatech/wwwroot/js/INST_PAD.js
@@ -1,8 +1,8 @@
 const toggleCheckbox = document.getElementById('checkbox_toggle');
-toggleCheckbox.addEventListener('change', function() {
-    const institucionalForm = document.getElementById('institucional_form');
+toggleCheckbox.addEventListener('change', function () {
     const padresForm = document.getElementById('padres_form');
-    
+    const institucionalForm = document.getElementById('institucional_form');
+     
     if (this.checked) {
         institucionalForm.style.display = 'block';
         padresForm.style.display = 'none';
