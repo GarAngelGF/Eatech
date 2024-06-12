6905e55539446a263a892a2e80794fff147a1696
GarAngelGF <agarciaf2102@alumno.ipn.mx>
2024-06-12 13:41:55 -0600

    Crud Pedido controller Acabado

--- Eatech/Controllers/PedidoController.cs
+++ Eatech/Controllers/PedidoController.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore.Authorization;
 using System.ComponentModel.Design;
 using NuGet.Protocol.Plugins;
 using System.Security.Cryptography;
+using Microsoft.EntityFrameworkCore;
+using SixLabors.ImageSharp.PixelFormats;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using NuGet.Versioning;
 
 
 namespace Eatech.Controllers
@@ -34,25 +38,110 @@ namespace Eatech.Controllers
         {
             return View();
         }
+        /*-Apartado para buscar alumnos antes del registro xd-*/
 
         /*-Task para crear pedido + enviar el correo de pedido creado-*/
-        public async Task <IActionResult> RegistrarPedido()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RegistrarPedido(Guid IdAlum, Guid IdCom, [Bind("pedido,FechaCPedido,FechaEntrega,NotaPedido,Estatus")] Bd_Pedido bd_Pedido, [Bind("pedido,IdAlumno")] BdI_Alu_Ped bdI_Alu_Ped, [Bind("IDComida,pedido")] BdI_Com_Ped bdI_Com_Ped)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                bd_Pedido.pedido = Guid.NewGuid();
+
+                bdI_Com_Ped.IDComida = IdCom;
+                bdI_Com_Ped.pedido = bd_Pedido.pedido;
+
+                bdI_Alu_Ped.pedido = bd_Pedido.pedido;
+                bdI_Alu_Ped.IdAlumno = IdAlum;
+                /*-aqui va para poner el correo pa avisar del pedido creado-*/
+
+                _context.Add(bdI_Alu_Ped);
+                _context.Add(bdI_Com_Ped);
+                _context.Add(bd_Pedido);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+
+            }
+            return View(bd_Pedido);
         }
+        /*-*/
 
 
         //**************************************************************************************************************************************************************************//
         /*-Apartado para editar el estatus del pedido por parte del admin + envio de correo al cliente para -*/
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> EditarPedido(Guid? Id)
+        {
+            if (Id == null || _context.Pedidos == null) return NotFound();
+            var ltam = await _context.Pedidos.FindAsync(Id);
+            if (ltam == null) return NotFound();
+            return View(ltam);
+        }
 
+        /*-Task para moificar el pedido-*/
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditarEstatus(Guid ID, [Bind("Estatus")] Bd_Pedido bd_pedido)
+        {
+            if (ID != bd_pedido.pedido) return NotFound();
+            try
+            {
+                _context.Update(bd_pedido);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PedidoEx(bd_pedido.pedido)) return NotFound();
+                else throw;
+            }
+            return View(bd_pedido);
+        }
+
+        /*-pedido Existente-*/
+        private bool PedidoEx(Guid id)
+        {
+            return (_context.Pedidos?.Any(licerv => licerv.pedido == id)).GetValueOrDefault();
+        }
 
         //**************************************************************************************************************************************************************************//
         /*-Apartado para eliminar pedidos mayores a 1 año para ahorrar almacenamiento en la base de datos [Solo admin]-*/
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> EliminarPedidos(Guid? Id)
+        {
+            if (Id == null || _context.Pedidos == null) return NotFound();
+            var ltam = await _context.Pedidos.FindAsync(Id);
+            if (ltam == null) return NotFound();
+            return View(ltam);
+        }
 
+        /*-Task para eliminar de la base de datos los pedidos-*/
+        public async Task<IActionResult> ConfirmarPedido(Guid id)
+        {
+            if (id == null || _context.Pedidos == null) return Problem("El pedido no existe");
+            var ltam = await _context.Intermedia_Comida_Pedi.FindAsync(id);
+            var lcv = await _context.Intermedia_Alum_Pedi.FindAsync(id);
+            var cc = await _context.Pedidos.FindAsync(id);
+            if (ltam != null && lcv != null && cc != null)
+            {
+                _context.Intermedia_Comida_Pedi.Remove(ltam);
+                _context.Intermedia_Alum_Pedi.Remove(lcv);
+                _context.Pedidos.Remove(cc);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
 
         //**************************************************************************************************************************************************************************//
         /*-Apartado para ver el pedido de manera individual-*/
-
+        public IActionResult PedidoDashboard(Guid? id)
+        {
+            if (id == null || _context.Pedidos == null) return NotFound();
+            var lgc = _context.Pedidos.Where(ltam => ltam.pedido == id);
+            if (lgc == null) return NotFound();
+            return View(lgc);
+        }
 
         //**************************************************************************************************************************************************************************//
         /*-apartado vistas de admin [dashboard, etc]-*/
