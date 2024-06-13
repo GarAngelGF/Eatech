0fd8eeba52ca0920d38b6fd179c621f3ec42ab52
GarAngelGF <agarciaf2102@alumno.ipn.mx>
2024-06-13 10:26:20 -0600

    correodos

--- Eatech/Utilerias/Correo.cs
+++ Eatech/Utilerias/Correo.cs
@@ -29,5 +29,29 @@ namespace Eatech.Utilerias
             return true;
 
         }
+        public static bool PedidoCorreo(string receptor, string asunto, string mensaje)
+        {
+            MailMessage mail = new MailMessage();
+
+            mail.From = new MailAddress("pedidos@eatech.me");
+            mail.To.Add(new MailAddress(receptor));
+            mail.Subject = asunto;
+            mail.Body = mensaje;
+            mail.IsBodyHtml = true;
+            mail.Priority = MailPriority.Normal;
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.ionos.mx";
+            smtp.Port = 587;
+            smtp.EnableSsl = true;
+            smtp.UseDefaultCredentials = false;
+
+            NetworkCredential credencial = new NetworkCredential("pedidos@eatech.me", "Sopadepapa22#");
+            smtp.Credentials = credencial;
+            smtp.Send(mail);
+
+            return true;
+
+        }
     }
 }
